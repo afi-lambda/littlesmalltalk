103ab68948c99d8fb29d458ef4de5172d7ae75bb
Alain Fischer <alain.fischer@bluewin.ch>
2017-12-26 13:44:38 +0100

    Separated ffibuilder from execution

--- lst3/st.py
+++ lst3/st.py
@@ -1,33 +1,6 @@
 from __future__ import print_function
-import cffi
-
-ffibuilder = cffi.FFI()
-ffibuilder.set_source('lst3_def', None)
-ffibuilder.cdef("void initMemoryManager();")
-ffibuilder.cdef("FILE *fopen(char*, char*);")
-ffibuilder.cdef("size_t fread(void*, size_t, size_t, FILE*);")
-ffibuilder.cdef("void imageRead(FILE*);")
-ffibuilder.cdef("void initCommonSymbols();")
-ffibuilder.cdef("typedef int object;")
-ffibuilder.cdef("object symbols;")
-ffibuilder.cdef("object nameTableLookup(object, char*);")
-ffibuilder.cdef("typedef int boolean;")
-ffibuilder.cdef("boolean execute(object, int);")
-ffibuilder.cdef("struct dummy {int di; object cl; short ds;} dummyObject;")
-ffibuilder.cdef("""struct objectStruct {
-    object cl;
-    short referenceCount;
-    short size;
-    object *memory;};""")
-ffibuilder.cdef("struct objectStruct objectTable[6500];")
-ffibuilder.cdef("object *mBlockAlloc(int);")
-ffibuilder.cdef("void visit(object);")
-ffibuilder.cdef("void setFreeLists();")
-ffibuilder.cdef("")
-ffibuilder.cdef("")
-ffibuilder.cdef("")
+from build_lst3_def import ffibuilder
 ffibuilder.compile()
-
 from lst3_def import ffi
 
 def image_read(fp):
